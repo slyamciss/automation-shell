afd98ad5676a96ec787495c787e6872e462e1e2a
Souleymane Cisse <47922946+slyamciss@users.noreply.github.com>
2022-08-31 10:20:58 -0400

    Update variables.sh

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 Challenge=#90daysOfChallenge
 TotalDays=90
-echo "hey everyone, welcome to the #Challenge course"
-echo "Be sure to stay with us for #TotalDays"
+echo "hey everyone, welcome to the $Challenge course"
+echo "Be sure to stay with us for $TotalDays"
